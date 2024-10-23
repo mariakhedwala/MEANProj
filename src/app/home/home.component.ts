6d1e54e01b2819c114d25c32fe99fa086bbe9042
Maria Khedwala <mariakhedwala@gmail.com>
2024-10-23 19:58:25 +0530

    product listing with pagination

---
+++ src/app/home/home.component.ts
@@ -0,0 +1,45 @@
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+import { Product, Products } from '../../types';
+import { ProductComponent } from '../components/product/product.component';
+import { ProductsService } from '../services/products.service';
+import { PaginatorModule } from 'primeng/paginator';
+
+@Component({
+  selector: 'app-home',
+  standalone: true,
+  imports: [ProductComponent, CommonModule, PaginatorModule],
+  templateUrl: './home.component.html',
+  styleUrl: './home.component.scss'
+})
+export class HomeComponent {
+
+  constructor(
+    private ProductsService: ProductsService
+  ) { }
+
+  products: Product[] = [];
+
+  totalRecords: number = 0;
+  rows: number = 5
+
+  onProductOutput(product: Product) {
+    console.log(product, 'output')
+  }
+
+  onPageChange(event: any) {
+    this.fetchProducts(event.page, event.rows);
+  }
+
+  fetchProducts(page: number, perPage: number) {
+    this.ProductsService.getProducts('http://localhost:3000/clothes', { page, perPage })
+      .subscribe((products: Products) => {
+        this.products = products.items;
+        this.totalRecords = products.total;
+      })
+  }
+
+  ngOnInit() {
+    this.fetchProducts(0, this.rows)
+  }
+}
